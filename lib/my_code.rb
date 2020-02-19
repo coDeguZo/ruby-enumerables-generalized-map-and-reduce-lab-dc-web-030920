a115ac6b61310352cacace77ad14797882178d3f
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-19 05:06:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,8 +10,9 @@ def map(source_array)
     return new_array
 end
 
-def reduce(source_array)
+def reduce(source_array, starting_value = 0)
   array = []
+  total = starting_value
   i = 0
   while i < source_array.count do
     
