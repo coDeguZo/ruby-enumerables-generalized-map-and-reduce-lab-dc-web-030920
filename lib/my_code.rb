3e02b23c8923ce51884527a3ccfa13ac70a312f2
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-19 05:21:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,13 +10,19 @@ def map(source_array)
     return new_array
 end
 
-def reduce(source_array, starting_value)
-  array = []
-  total = starting_value
-  i = 0
-  while i < source_array.count do
-    array << yield(source_array[i], total)
-    i += 1
-  end
-  return array
+
+def reduce(array, starting_value = nil)
+   if starting_value
+    sum_num = starting_value
+    i = 0
+   else
+    sum_num = array[0]
+    i = 1
+   end
+
+   while i < array.count do
+    sum_num = yield(sum_num, array[i])
+   end
+   return sum_num
 end
+
