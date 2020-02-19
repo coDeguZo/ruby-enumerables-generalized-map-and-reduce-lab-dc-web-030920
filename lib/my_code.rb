b592c2a299c1685b3cabe0b9d9cd9e1db3d94f51
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-19 05:02:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,8 +4,7 @@ def map(source_array)
     new_array = []
     i = 0
     while i < source_array.count do
-        yield(source_array[i])
-        new_array << source_array[i]
+        new_array << yield(source_array[i])
         i += 1
     end
     return new_array
